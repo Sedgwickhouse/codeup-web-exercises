64d5f746b799d9fc27ec685f0d28a2fe87fa6247
Sedgwick House <sedgwick.house20@gmail.com>
2020-09-17 16:57:06 -0500

    refactoring code on weather map project

--- Js/weather_map.js
+++ Js/weather_map.js
@@ -2,6 +2,6 @@ mapboxgl.accessToken = MAPBOX_TOKEN;
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: [-84.394513, 33.766266], // starting position [lng, lat]
     zoom: 4 // starting zoom
 });
